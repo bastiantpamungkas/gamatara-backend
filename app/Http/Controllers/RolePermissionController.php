167aaefa0f82c45e9e72cf2ba1d116a18a17b3ce
zhofran <zhofranwijayanto@gmail.com>
2024-11-14 12:59:02 +0700

    Update for role and permission

---
+++ app/Http/Controllers/RolePermissionController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionController extends Controller
+{
+    public function role()
+    {
+        $role = Role::all();
+        
+        return response()->json([
+            'message' => 'Successfully get all roles',
+            'roles' => $role,
+        ], 200);
+    }
+
+    public function permission()
+    {
+        $permission = Permission::all()->groupBy('category');
+        
+        return response()->json([
+            'message' => 'Successfully get all permissions',
+            'permissions' => $permission,
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'permissions' => 'array'
+        ]);
+
+        $role = Role::create(['name' => $request->name]);
+
+        if ($request->has('permissions')) {
+            $role->syncPermissions($request->permissions);
+        }
+
+        return response()->json([
+            'message' => 'Role created successfully',
+            'role' => $role,
+            'permissions' => $role->permissions
+        ], 201);
+    }
+
+    public function editRole($id) 
+    {
+        $role = Role::findOrFail($id);
+        $permissions = Permission::all()->groupBy('category');
+        $rolePermission = $role->permissions->pluck('id')->toArray();
+        
+        return response()->json([
+            'status' => 'Success',
+            'role' => $role,
+        ], 200);
+    }
+
+    public function updateRole(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        $role = Role::findOrFail($id);
+        $role->name = $request->name;
+        $role->save();
+
+        return response()->json([
+            'message' => 'Role updated successfully',
+            'role' => $role,
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FaceScanController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\RolePermissionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,13 @@ Route::post('/', [FaceScanController::class, 'facescan']);
 
 Route::post('login', [AuthController::class, 'login']);
 
+Route::get('roles', [RolePermissionController::class, 'role']);
+Route::get('edit-role/{id}', [RolePermissionController::class, 'editRole']);
+
+Route::get('permissions', [RolePermissionController::class, 'permission']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('list-karyawan', [UserController::class, 'list']);
 
-    Route::get('list-role', [RoleController::class, 'index']);
     // Router lainnya
 });
